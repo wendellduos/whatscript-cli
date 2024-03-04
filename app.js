d77f49db3088f820ad87f063279ca76664dda80a
Wendel <wendellduos@hotmail.com>
2024-03-04 17:32:06 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const qrcode = require("qrcode-terminal");
+
+const { Client } = require("whatsapp-web.js");
+const client = new Client();
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+
+  sendMessageToMyself();
+});
+
+client.initialize();
+
+async function sendMessageToMyself() {
+  const allContacts = await client.getContacts();
+
+  let myId;
+
+  allContacts.forEach((contact) => {
+    if (contact.isMe) {
+      myId = contact.id._serialized;
+    }
+  });
+
+  client.sendMessage(myId, "test message");
+}
