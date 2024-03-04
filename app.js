d9c4e5b113b4cb51a09489107564ed8b7aff585f
Wendel <wendellduos@hotmail.com>
2024-03-04 17:39:40 -0300

    added --no-sandbox option

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const qrcode = require("qrcode-terminal");
 
 const { Client } = require("whatsapp-web.js");
-const client = new Client();
+const client = new Client({
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+});
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
